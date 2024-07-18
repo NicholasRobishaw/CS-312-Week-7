a1dd3a1550d9ec9a1f9a289dddf253a0bbfc3812
“Nicholas <nicholas.robishaw1@gmail.com>
2024-07-18 14:31:32 -0700

    first commit

---
+++ using-pre-built-react-components-forked/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import BackpackIcon from "@mui/icons-material/Backpack";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <BackpackIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
